27c1cc4cc4f43ba645992bc721eda2e27fc2c290
alekseishmidko <118507792+alekseishmidko@users.noreply.github.com>
2024-05-26 22:52:23 +0300

    1:23

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,12 +6,19 @@ import {
   HttpStatus,
   Post,
   Res,
+  UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { SignInBodyDto, SignUpBodyDto } from './dto/auth.dto';
+import {
+  GetSessionInfoDto,
+  SignInBodyDto,
+  SignUpBodyDto,
+} from './dto/auth.dto';
 import { ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
 import { Response } from 'express';
 import { CookieService } from './cookie.service';
+import { AuthGuard } from './auth.guard';
+import { SessionInfo } from './session-info.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -35,17 +42,30 @@ export class AuthController {
   @Post('sign-in')
   @ApiOkResponse()
   @HttpCode(HttpStatus.OK)
-  signIn(@Body() dto: SignInBodyDto) {
-    return null;
+  async signIn(
+    @Body() dto: SignInBodyDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { accessToken } = await this.authService.signIn(
+      dto.email,
+      dto.password,
+    );
+    this.cookieService.setToken(res, accessToken);
   }
 
   @Post('sign-out')
   @ApiOkResponse()
-  signOut() {}
+  @UseGuards(AuthGuard)
+  signOut(@Res({ passthrough: true }) res: Response) {
+    this.cookieService.removeToken(res);
+  }
 
   @Get('session')
-  @ApiOkResponse()
-  getSessionInfo() {
-    // return this.authService.getSessionInfo();
+  @ApiOkResponse({
+    type: GetSessionInfoDto,
+  })
+  @UseGuards(AuthGuard)
+  getSessionInfo(@SessionInfo() session: GetSessionInfoDto) {
+    return session;
   }
 }
